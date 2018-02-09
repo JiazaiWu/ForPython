26897a0d5451d7aa6d8ba5f135d92ac17b94f3a2
jiazai_wu <jiazai_wu@htc.com>
2018-02-09 15:05:20 +0800

    final cl get tool for git

--- web/PullGit.py
+++ web/PullGit.py
@@ -1,87 +1,68 @@
-# -*- coding:utf-8 -*-  
-import requests  
-import re  
-''''' 
-用到的库： 
-requests 
-re(正则) 
-流程： 
-手动登录查看需要的参数，观察一次登录发送的请求 
-               ***** 
-                *** 
-                 * 
-发现除了一个token是动态的，其他的都是输入或是固定的。参数如下： 
-'login':'。。。', 
-'password':'。。。', 
-'authenticity_token':token, 
-'commit':'Sign in', 
-'utf8': "✓"} 
-               ***** 
-                *** 
-                 * 
-用正则在登录页面爬取登录用的token,并赋值到参数里 
-               ***** 
-                *** 
-                 * 
-接着用会话对象去发送带有参数的数据到url 
-               ***** 
-                *** 
-                 * 
-                完成 
-'''  
+import requests
+import sys
+import io
+import re
+import json
 
-'''读文件获得git列表
-'''
-ClList = []
-gitList = []
-with open("gitList", "r") as inputfile:
-    for line in inputfile:
-        gitList.append(line.replace("\n", ""))
-inputfile.close()
 
-s = requests.Session()#获取会话对象，用于登录时的cookie和session  
-header = {'Host': 'github.com',  
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0',  
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',  
-          'Accept-Language': 'en-US,en;q=0.5',  
-          'Accept-Encoding': 'gzip, deflate, br',  
-          'Referer': 'https://github.com',  
-          'Connection': 'keep-alive'}  
-  
-indexUrl = 'https://github.com/'#首页地址  
-loginUrl = 'https://github.com/login'#登录页面  
-sessionUrl = 'https://github.com/session'#登录页面把表单提交到这个url，然后重定向到首页，把数据提交到这个Url之后可以获取到cookie,用于登录 
-##>>> payload = {'key1': 'value1', 'key2': 'value2'}  
-##>>> r = requests.get("http://httpbin.org/get", params=payload)  
-  
-tokenRule = '<input name="authenticity_token" type="hidden" value="(.*?)"'#登录参数里面有一个token参数，是加载到页面中并且是动态的，需要爬出来  
-loginHtml = s.get(loginUrl,headers=header)#得到登录页面  
-tokenPattern = re.compile(tokenRule,re.S)  
-arraryTolen = re.findall(tokenPattern,loginHtml.text)#得到动态的token,这里返回的事数组，取第一个  
-  
-#print(loginHtml.text)  
-token = arraryTolen[0]  
-print("the token is: ",token)  
-  
-loginParams = {'login':'wujiazaiwh@163.com',  
-               'password':'myhtc201481',
-               'authenticity_token':token,  
-               'commit':'Sign in',  
-               'utf8': "✓"}#配置登录参数  
-sessionHtml = s.post(sessionUrl,data=loginParams)#得到登录后的页面，其实被定位到了首页  
-''''' 
-输出是否登录成功，如果不成功返回状态码和原因，以及详细的错误页面 
-'''  
-#print("the session rsult is: ",sessionHtml.status_code)  
-#print("the reason is: ",sessionHtml.reason)  
-#print("the detail is: ",sessionHtml.text)  
-  
-indexhtml = s.get('https://github.com/HanlaoTwo/StacknotOverFlow', headers=header)#可以继续用这个会话对象去访问需要登录的页面  
-#print(indexhtml.text)  
+cookie_str = r'''s_fid=308606A89767EE44-049182C0C6CF190B; s_vi=[CS]v1|2B1314DF851D1A76-4000015160324A26[CE]; AMCV_327761B652606ED30A490D4C%40AdobeOrg=793872103%7CMCAAMB-1465438727%7CNRX38WO0n5BH8Th-nqAG_A%7CMCAAMLH-1464929700%7C11%7CMCAID%7C2B1314DF851D1A76-4000015160324A26%7CMCIDTS%7C16955%7CMCMID%7C29129975055344574525935256751111319268; _ga=GA1.2.1362505539.1445341633; pgv_pvi=1546051584; GerritAccount=aOYDprq8yKLOzpK1UYN7FJTvtHqQ99N7rW'''
+xauth = 'aOYDprrREZk.Ir1Kn3nTSHEQRK4eBMXq4q'
+inputfile = 'giturl'
+outputfile = 'cl'
 
-#写文件
-with open("CLid", "w") as outputfile:
-    for line in gitList:
-        outputfile.write(line)
-        outputfile.write("\n")
-outputfile.close()
+cookies = {}
+for line in cookie_str.split(';'):
+    key, value = line.split('=', 1)
+    cookies[key] = value
+
+requesthead = {
+'Host': 'git.htc.com:8081',
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Language': 'en-US,en;q=0.5',
+'Accept-Encoding': 'gzip, deflate',
+'X-Gerrit-Auth': xauth,
+'Referer': 'http://git.htc.com:8081/',
+'Connection': 'keep-alive'
+}
+
+
+cllist = []
+with open(inputfile, "r") as f:
+  for line in f:
+    #form url
+    if len(line) < 24:
+      break
+    if line[-1] == "\n":
+      line = line[:-1]
+    line = line.replace(r'#/c/', "")
+    line = line[:24] + "changes/" + line[24:]
+    if line[-1] == r'/':
+      line = line[:-1]
+    line = line + "/detail"
+    #it should be like:  http://git.htc.com:8081/changes/1009763/detail
+    print(line)
+    
+    #get git info
+    response = requests.get(line, headers = requesthead, cookies = cookies)
+
+    # to handle hit git response abnormal
+    clid = 0
+    if response.text.find("Not") != -1:
+      clid = 0
+    else:
+      # error char )]}'\n ahead of json data
+      jstring = response.text[5:]
+      # store cl
+      js = json.loads(jstring)
+      clid = js['clid']
+
+    cllist.append(clid)
+f.close()
+
+print(cllist)
+
+with open(outputfile, "w") as f:
+  for index in range(len(cllist)):
+    f.write(str(cllist[index]))
+    f.write('\n')
+f.close()
